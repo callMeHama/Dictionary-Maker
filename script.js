eea38ad98a7a85216fdd60e406fb4e7a40959baa
callMeHama <callMeHama>
2024-09-09 14:16:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Tab') {
+        if (document.activeElement === document.querySelectorAll("input")[document.querySelectorAll("input").length-1]){
+            let child = document.createElement("input");
+            child.type = "text";
+            child.className = "1";
+            let child2 = document.createElement("input");
+            child2.type = "text";
+            child2.className = "2";
+
+            document.querySelector('div').appendChild(child);
+            document.querySelector('div').appendChild(child2);  
+        }
+    }
+});
+
+function dictCreate(){
+    let output = "{";
+    inp1 = document.getElementsByClassName('1');
+    inp2 = document.getElementsByClassName('2');
+
+    for (let i = 0; i < inp1.length; i++){
+        output+= "<br>&nbsp;&nbsp;&nbsp;&nbsp;\""+inp1[i].value+'\" : \"'+inp2[i].value+"\"";
+    }
+    output+="<br>}";
+    console.log(output);
+    document.querySelector('h1').innerHTML = output;
+}
